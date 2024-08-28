846bdad76ebfa473f11458b7fbdaa789146b97f3
Sandeep H.T <harikrishnansandeep10@gmail.com>
2024-08-28 10:55:02 +0530

    added a function createGrid which contains the loop to create a nXn grid,also added event listener to track the grid being hovered over

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 const container=document.querySelector(".container");
-let cell;
-let num=1;
-let column;
-for(let i=0;i<16;i++){
-    column=document.createElement("div")
-    
-    for(let j=0;j<16;j++){
-        cell=document.createElement("div")
-        cell.innerHTML=` <button onClick="handleClickChange()">
-                            Click Button ${num++}
-                        </button>`
-        column.appendChild(cell)
-        column.style="display: flex"
-        container.appendChild(column);
-        if(j%2==0){
-            cell.style="border: 1px solid black;height: 50px;width: 50px; color:black;    background-color: antiquewhite;"
-        }
-        else{
-            cell.style="border: 1px solid black;height: 50px;width: 50px; color:red;     background-color: antiquewhite;"
-        }
-        
+let createGrid=(num)=>{
+    for(let i=0;i<num;i++){
+        const column=document.createElement("div");
+
+        for(let j=0;j<num;j++){
+        const cell=document.createElement("div");
+        cell.style="border:1px solid black;height:50px;width:50px;background-color:white";
+        cell.addEventListener("mouseover",()=>{
+            cell.style="border:1px solid black;height:50px;width:50px;background-color:black";
+
+        })
+        column.appendChild(cell);
+    }
+    container.append(column);
         
     }
-}
 
-function handleClickChange(){
-    cell.style="color:white;     background-color: black;"
+
 }
+createGrid(16);
+    
+    
+
+
+
+
+
+
